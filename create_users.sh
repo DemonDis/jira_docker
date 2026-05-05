7bc6ee4bf796382c6a3c74a8d7a0b97b062b3fdb
Дмитрий Шибикин <sdd@gitlab.lab.local>
2026-05-05 11:50:39 +0700

    🔥 Remove duplicate admin creation code from create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -24,31 +24,6 @@ curl -s -c "$COOKIE_JAR" -X POST "$JIRA/rest/auth/1/session" \
 # Получение CSRF токена
 curl -s -b "$COOKIE_JAR" -c "$COOKIE_JAR" "$JIRA/secure/Dashboard.jspa" -o /dev/null
 
-# Создание администратора
-echo "Создание администратора..."
-curl -s -b "$COOKIE_JAR" -X POST "$JIRA/rest/api/2/user" \
-  -H "Content-Type: application/json" \
-  -H "X-Atlassian-Token: no-check" \
-  -d '{
-  "name": "superadmin",
-  "password": "superadmin123",
-  "emailAddress": "superadmin@example.com",
-  "displayName": "Super Administrator"
-}'
-
-# Добавление в группу администраторов
-curl -s -b "$COOKIE_JAR" -X POST "$JIRA/rest/api/2/group/user?groupname=jira-administrators" \
-  -H "Content-Type: application/json" \
-  -H "X-Atlassian-Token: no-check" \
-  -d '{"name": "superadmin"}'
-
-echo "Администратор superadmin создан"
-
-# Создание обычных пользователей
-USERS=(
-  "lsa:lsa lsa:lsa@lsa.lsa"
-)
-
 for user in "${USERS[@]}"; do
   IFS=':' read -r username displayname email <<< "$user"
   echo "Создание пользователя: $username"
@@ -69,33 +44,34 @@ rm -f "$COOKIE_JAR"
 
 echo "=== Готово! ==="
 echo "Созданные пользователи:"
-echo "  superadmin / superadmin123 (администратор)"
 for user in "${USERS[@]}"; do
   IFS=':' read -r username displayname email <<< "$user"
   echo "  $username / $username"
 done
 echo "Jira доступна!"
 
-# Создание администратора
-echo "Создание администратора..."
-curl -s -X POST -H "Content-Type: application/json" -u "$ADMIN_USER:$ADMIN_PASS" \
-  "$JIRA/rest/api/2/user" -d '{
-  "name": "admin",
-  "password": "123",
-  "emailAddress": "superadmin@example.com",
-  "displayName": "Super Administrator"
-}'
-
-# Добавление в группу администраторов
-curl -s -X POST -H "Content-Type: application/json" -u "$ADMIN_USER:$ADMIN_PASS" \
-  "$JIRA/rest/api/2/group/user?groupname=jira-administrators" \
-  -d '{"name": "superadmin"}'
-
-echo "Администратор superadmin создан"
-
 # Создание обычных пользователей
 USERS=(
-  "lsa:lsa lsa:lsa@lsa.lsa"
+    "lsa: lsa:lsa@lsa.lsa"
+    "bke: bke:bke@xxx.xxx"
+    "bma: bma:bma@xxx.xxx"
+    "cna: cna:cna@xxx.xxx"
+    "dap: dap:dap@xxx.xxx"
+    "gsa: gsa:gsa@xxx.xxx"
+    "knv: knv:knv@xxx.xxx"
+    "lsa: lsa:lsa@xxx.xxx"
+    "mee: mee:mee@xxx.xxx"
+    "ptl: ptl:ptl@xxx.xxx"
+    "ssl: ssl:ssl@xxx.xxx"
+    "sav: schav:schav@xxx.xxx"
+    "sii: sii:sii@xxx.xxx"
+    "sre: sre:sre@xxx.xxx"
+    "voi: voi:voi@xxx.xxx"
+    "vev: vev:vev@xxx.xxx"
+    "yvi: yvi:yvi@xxx.xxx"
+    "daa: daa:daa@xxx.xxx"
+    "kgv: kgv:kgv@xxx.xxx"
+    "iea: iea:iea@xxx.xxx"
 )
 
 for user in "${USERS[@]}"; do
@@ -113,8 +89,7 @@ done
 
 echo "=== Готово! ==="
 echo "Созданные пользователи:"
-echo "  superadmin / superadmin123 (администратор)"
 for user in "${USERS[@]}"; do
   IFS=':' read -r username displayname email <<< "$user"
-  echo "  $username / $username"
+  echo "  $username"
 done
